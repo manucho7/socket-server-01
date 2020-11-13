0174ac86836b34222f9929db3d2eee8641c933bc
manucho <manuel.blanco77@gmail.com>
2020-11-13 15:38:19 -0300

    Conectando un cliente (index.html) con nuestro backend socket server

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const app = require('express')()
+
+const express = require('express');
+const app = express();
+
 const server = require('http').createServer(app);
+
 const io = require('socket.io')(server);
 
+//desplegar el directorio publico
+app.use(express.static(__dirname + '/public'));
 
-io.on('connection', () => {});
+
+io.on('connection', () => {
+    console.log('Cliente conectado')
+});
+
+
 server.listen(3000, () => {
-    console.log('Server corriendo en puerto : 8080');
+    console.log('Server corriendo en puerto : 3000');
 });
 
